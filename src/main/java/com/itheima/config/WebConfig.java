bd858a5f89f86a39d607a6883eb414c92acd4b79
abai <1349131840@qq.com>
2024-04-17 20:02:06 +0800

    合并主分支

--- src/main/java/com/itheima/config/WebConfig.java
+++ src/main/java/com/itheima/config/WebConfig.java
@@ -14,7 +14,8 @@ public class WebConfig implements WebMvcConfigurer {
 
     public void addInterceptors(InterceptorRegistry registry) {
         //登录和注册接口不拦截
-        // adadad
+        // adadadwerf
+        // asfwaw
         registry.addInterceptor(loginInterceptor).excludePathPatterns("/user/login","/user/register","/user/token","/advertisingspace");
     }
 }
